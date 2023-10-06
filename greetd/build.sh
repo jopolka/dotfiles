cb0363609e720cd0af17501fe6ec2c5397a47692
jopolka <joerg.opolka@gmail.com>
2023-10-06 15:23:05 +0200

    fixed greetd

--- greetd/build.sh
+++ greetd/build.sh
@@ -2,7 +2,8 @@
 git clone https://github.com/kennylevinsen/greetd build_greetd
 cd build_greetd
 cargo build --release
-sudo cp target/release/{greetd,agreety} /usr/local/bin/
+sudo cp target/release/greetd /usr/local/bin/
+sudo cp target/release/agreety /usr/local/bin/
 sudo cp greetd.service /etc/systemd/system/greetd.service
 cd ..
 rm -rf build_greetd
