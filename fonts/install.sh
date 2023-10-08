5cc10cf690f88fbeabe5c3719356d950bbb55bb2
jopolka <joerg.opolka@gmail.com>
2023-10-08 23:51:49 +0200

    bugfix for fonts

--- fonts/install.sh
+++ fonts/install.sh
@@ -5,6 +5,7 @@ curl -L https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%
 curl -L https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf --output MesloLGS\ NF\ Bold.ttf
 curl -L https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf --output MesloLGS\ NF\ Italic.ttf
 curl -L https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20fItalic.ttf --output MesloLGS\ NF\ Bold\ Italic.ttf
+mkdir -p ~/.fonts
 cp *.ttf ~/.fonts/
 fc-cache -fv
 cd ..
