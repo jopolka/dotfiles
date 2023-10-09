3e7df4175fd456570ced7793a55a6761cbeed5a4
jopolka <joerg.opolka@gmail.com>
2023-10-09 20:32:50 +0200

    fixed fonts

--- fonts/install.sh
+++ fonts/install.sh
@@ -7,6 +7,20 @@ curl -L https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%
 curl -L https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20fItalic.ttf --output MesloLGS\ NF\ Bold\ Italic.ttf
 mkdir -p ~/.fonts
 cp *.ttf ~/.fonts/
-fc-cache -fv
+
+git clone https://github.com/stark/siji && cd siji
+./install.sh
+cd ..
+
+curl -L https://use.fontawesome.com/releases/v5.15.4/fontawesome-free-5.15.4-desktop.zip
+unzip fontawesome-free-5.15.4-desktop.zip
+cd fontawesome-free-5.15.4-desktop
+cp Font\ Awesome\ 5\ Free-Regular-400.otf ~/.local/share/fonts/
+mkdir -p ~/.fonts
+cp *.oft ~/.fonts/
+
+cd ..
+
 cd ..
 rm -rf install_fonts
+fc-cache -fv
