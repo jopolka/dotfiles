1d085259352f25a18ca2a2edf183ba012eef0f6a
jopolka <joerg.opolka@gmail.com>
2023-10-13 10:53:00 +0200

    fixed zsh install

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
-sudo apt install zsh -y
+sudo apt install zsh tmux -y
 ln -s -f "$(pwd)"/zsh/.zshrc ~/.zshrc
+
 sh -c "ZSH=$(pwd)/zsh/.oh-my-zsh $(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/themes/powerlevel10k
+ln -s "$(pwd)"/zsh/.oh-my-zsh ~/.oh-my-zsh
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$(pwd)"/zsh/.oh-my-zsh/custom/themes/powerlevel10k
 ln -s "$(pwd)"/zsh/.p10k.zsh ~/.p10k.zsh
+
 chsh -s "$(which zsh)"
