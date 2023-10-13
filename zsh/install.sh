3c10acc7c8fdc3ae98707e261397b953f793fe88
jopolka <joerg.opolka@gmail.com>
2023-10-13 11:17:38 +0200

    fixed chsh asking for password

--- zsh/install.sh
+++ zsh/install.sh
@@ -8,4 +8,4 @@ ln -s "$(pwd)"/zsh/.oh-my-zsh ~/.oh-my-zsh
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$(pwd)"/zsh/.oh-my-zsh/custom/themes/powerlevel10k
 ln -s "$(pwd)"/zsh/.p10k.zsh ~/.p10k.zsh
 
-chsh -s "$(which zsh)"
+sudo chsh -s "$(which zsh)" "$(whoami)"
