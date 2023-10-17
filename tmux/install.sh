46ba6616d10496284d3bcd0b3d00fbf62633b804
jopolka <joerg.opolka@gmail.com>
2023-10-17 23:06:31 +0200

    fixed tmux installation

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 sudo apt-get install -y tmux
-git clone https://github.com/tmux-plugins/tmux-tpm "$(pwd)"/tmux/plugins/tpm
+git clone https://github.com/tmux-plugins/tpm "$(pwd)"/tmux/plugins/tpm
 git clone https://github.com/tmux-plugins/tmux-resurrect "$(pwd)"/tmux/plugins/tmux-resurrect
 git clone https://github.com/tmux-plugins/tmux-sensible "$(pwd)"/tmux/plugins/tmux-sensible
 
-ln -s "$(pwd)"/tmux ~/.tmux
-ln -s "$(pwd)"/tmux/.tmux.conf ~/.tmux.conf
+ln -sf "$(pwd)"/tmux ~/.tmux
+ln -sf "$(pwd)"/tmux/.tmux.conf ~/.tmux.conf
